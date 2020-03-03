61ff1f48503f1849b5d29dc48ad986b515ec11e4
key2-hue <keymaeda3782@gmail.com>
2020-03-03 23:33:33 +0900

    03/03 23:33

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors',1);
+
+define('DSN','');
+define('DB_USERNAME','dbuser');
+define('DATABASE_PASSWORD','keymaeda2');
+define('DATABASE_NAME','poll_git_php');
+
+session_start();
